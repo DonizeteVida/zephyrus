413bc55f2dcd12781f0c59a107595e9e42a68356
DonizeteVida <donizetevida1998@outlook.com>
2026-04-03 16:45:43 +0200

    Add input support

--- src/main.c
+++ src/main.c
@@ -7,10 +7,16 @@ LOG_MODULE_REGISTER(app);
 
 #include <lvgl.h>
 
+static void button_clicked_cb(lv_event_t * event)
+{
+	LOG_INF("Button event: %s", lv_event_code_get_name(lv_event_get_code(event)));
+}
+
 int main(void)
 {
 	const struct device *display_dev;
 	lv_obj_t *label;
+	lv_obj_t *button;
 	int ret;
 
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
@@ -19,9 +25,12 @@ int main(void)
 		return 0;
 	}
 
-    label = lv_label_create(lv_screen_active());
+	button = lv_button_create(lv_screen_active());
+	lv_obj_add_event_cb(button, button_clicked_cb, LV_EVENT_CLICKED, NULL);
+	lv_obj_center(button);
+
+    label = lv_label_create(button);
 	lv_label_set_text(label, "Hello world!");
-	lv_obj_center(label);
 
 	lv_timer_handler();
 	ret = display_blanking_off(display_dev);
